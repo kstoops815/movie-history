3894b02119a429b92f5107755f29f843c550cb94
Kelly Stoops <kstoops815@gmail.com>
2017-10-23 21:00:51 -0500

    user authentication, closes #19

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const tmdb = require("./tmdb");
+const firebaseApi = require("./firebaseApi");
 
 const pressEnter = () => {
 	$(document).keypress((e) => {
@@ -32,4 +33,18 @@ const myLinks = () => {
 	});
 };
 
-module.exports = {pressEnter, myLinks};
+const googleAuth = () => {
+	$("#googleButton").click((e) => {
+		firebaseApi.authenticateGoogle().then((result) => {
+			console.log("result", result);
+		}).catch((error) => {
+			console.log("error in authenticateGoogle", error);
+		});
+	});
+};
+
+
+
+
+
+module.exports = {pressEnter, myLinks, googleAuth};
